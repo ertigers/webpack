276734b36f5e22dcd4649bbb5c4bad3d4424778f
lvyihu <1720427782@qq.com>
2023-04-18 16:04:13 +0800

    update

---
+++ webpack-demo/src/function/index.js
@@ -0,0 +1,7 @@
+const test = ()=> {
+  console.log('这里是utils/index文件');
+}
+
+const name = '吕宜虎'
+
+export { test, name }
---
+++ webpack-demo/src/index.js
@@ -0,0 +1,7 @@
+import { test, name } from './function/index.js'
+
+const webcu2plugin = {
+  test, name
+}
+
+export default webcu2plugin
